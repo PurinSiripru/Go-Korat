a0d2ff091785e1e9e9fc2fcfb523ac87e20862c8
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-08-28 10:23:00 +0700

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,39 +16,6 @@ const Sidebar = () =>{
     <div className="sidebar">
         <div className="top">
             <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
-            <div onClick={()=>newChat()} className="new-chat">
-                <img src={assets.plus_icon} alt="" />
-                {extended?<p>แชทใหม่</p>:null}
-            </div>
-            {extended?
-            <div className="recent">
-                <p className="recent-title">ค้นหาล่าสุด</p>
-                {prevPrompts && prevPrompts.map((item,index)=>{
-                    return (
-                        <div onClick={()=>loadPrompt(item)} className="recent-entry">
-                            <img src={assets.message_icon} alt="" />
-                            <p>{item.slice(0,18)} ...</p>
-                        </div>
-                    )
-                })}
-                
-            </div>
-            :null
-            }
-        </div>
-        <div className="bottom">
-            <div className="bottom-item recent-entry">
-                <img src={assets.question_icon} alt="" />
-                {extended?<p>ช่วยเหลือ</p>:null}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" />
-                {extended?<p>ประวัติการค้นหา</p>:null}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.setting_icon} alt="" />
-                {extended?<p>ตั้งค่า</p>:null}
-            </div>
         </div>
     </div>
   )
